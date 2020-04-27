fa489d056a46ecfac82cdfd2f3024aa7f82a9af4
Asikur <asikurrahman7@gmail.com>
2020-04-27 19:03:09 +0600

    Updates

---
+++ precache-manifest.8ddcf806a0643dc8b379c7b9795c8e5e.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7928e03b61b05da830de27c96618b21",
+    "url": "/react-business-website/index.html"
+  },
+  {
+    "revision": "f02e8dc8bc801fd40cb5",
+    "url": "/react-business-website/static/css/2.fe747e67.chunk.css"
+  },
+  {
+    "revision": "61e00679d81e4ce42325",
+    "url": "/react-business-website/static/css/main.444902b5.chunk.css"
+  },
+  {
+    "revision": "f02e8dc8bc801fd40cb5",
+    "url": "/react-business-website/static/js/2.e9a57e71.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/react-business-website/static/js/2.e9a57e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e00679d81e4ce42325",
+    "url": "/react-business-website/static/js/main.2c7e5ccb.chunk.js"
+  },
+  {
+    "revision": "bd576472defb7a1e3df1",
+    "url": "/react-business-website/static/js/runtime-main.a54de745.js"
+  },
+  {
+    "revision": "bb9c696353e219dc99f526c0b1b9b197",
+    "url": "/react-business-website/static/media/1.bb9c6963.jpg"
+  },
+  {
+    "revision": "8570eed727fdf857592fcf380a2fa000",
+    "url": "/react-business-website/static/media/2.8570eed7.jpg"
+  },
+  {
+    "revision": "3523849722395b3a8278d92967aa53d3",
+    "url": "/react-business-website/static/media/3.35238497.jpg"
+  },
+  {
+    "revision": "1a15e1ac4778263eba9a603bbe46e81d",
+    "url": "/react-business-website/static/media/4.1a15e1ac.jpg"
+  },
+  {
+    "revision": "444246bb6b228069cfe615b657c79eac",
+    "url": "/react-business-website/static/media/AboutBnr.444246bb.jpg"
+  },
+  {
+    "revision": "c438aabb8bff649a61287eda2e788876",
+    "url": "/react-business-website/static/media/about1.c438aabb.jpg"
+  },
+  {
+    "revision": "14bd77af78c50a45f296fbeae5ee6974",
+    "url": "/react-business-website/static/media/about2.14bd77af.jpg"
+  },
+  {
+    "revision": "e9e63e565f15e6f1d32c49d840ca552c",
+    "url": "/react-business-website/static/media/aboutsection.e9e63e56.jpg"
+  },
+  {
+    "revision": "23d6f0e0badc2b431b65e88b6dd2b635",
+    "url": "/react-business-website/static/media/blog1.23d6f0e0.jpg"
+  },
+  {
+    "revision": "e12c6af406f67fa71d23aed081e6860d",
+    "url": "/react-business-website/static/media/blog2.e12c6af4.jpg"
+  },
+  {
+    "revision": "d9fb31558a507e7e7b902340b5f560e2",
+    "url": "/react-business-website/static/media/services_img.d9fb3155.jpg"
+  },
+  {
+    "revision": "00abb5e24ce739511fe01fceb2539d64",
+    "url": "/react-business-website/static/media/skill.00abb5e2.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-business-website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-business-website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-business-website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-business-website/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7790ece73a15003a60c1c8179c86abbd",
+    "url": "/react-business-website/static/media/slider1.7790ece7.jpg"
+  },
+  {
+    "revision": "1f617fd85876350f62d7cc7de58587c2",
+    "url": "/react-business-website/static/media/slider2.1f617fd8.jpg"
+  },
+  {
+    "revision": "47ee3e37f10fc10f2e349e9d7678769d",
+    "url": "/react-business-website/static/media/slider3.47ee3e37.jpg"
+  },
+  {
+    "revision": "6833ecb372f1d6eb3b8072773a6f4ef2",
+    "url": "/react-business-website/static/media/testimonial.6833ecb3.jpg"
+  }
+]);
